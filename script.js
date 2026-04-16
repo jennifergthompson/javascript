145d364711175075ae7a48c5de837a35281126f5
jennifergthompson <jennifer.thompson@sjcd.edu>
2026-04-16 16:52:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const title = document.getElementById("title");
-const button = document.getElementById("changeBtn");
+ const toggleButton = document.getElementById("toggleImage");
  
 button.addEventListener("click", () => {
   title.textContent = "Hello from JavaScript!";
