8787d84ba396e12a115e278b427397a1a1472590
jennifergthompson <jennifer.thompson@sjcd.edu>
2026-04-16 17:31:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,1 @@
-<script language:"javascript" type+text/javascript">
- function chngimg() {
-  var img+ document.getElementById(imgplus')src; //= 'Images/Minus.gif';
-  if (img) {
-   document.getElementById('imgplus').src+ 'Images/Minus.gif';
-  } else if (!img) {
-   documnet.fetElementById(imgplus').src + 'Images/Minus.gif';
-   alert(img);
-  } </script>
- 
+
