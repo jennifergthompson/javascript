6ed87b3f5a442701adef609410eb1a397f3d74fc
jennifergthompson <jennifer.thompson@sjcd.edu>
2026-04-16 16:40:25 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-
+const title = document.getElementById("title");
+const button = document.getElementById("changeBtn");
+ 
+button.addEventListener("click", () => {
+  title.textContent = "Hello from JavaScript!";
+  title.style.color = "teal";
+});
